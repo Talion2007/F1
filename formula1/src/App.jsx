49034b260384b006d58f2949b95c45861b0c829a
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-03-20 14:47:56 -0300

    V2.01

--- formula1/src/App.jsx
+++ formula1/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Home from './pages/Home';
 import Drivers from './pages/Drivers';
 import Sprints from './pages/Sprints';
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
     <Analytics/>
+    <SpeedInsights/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
