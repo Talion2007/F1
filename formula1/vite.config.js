e87a587ea667444731188c18c00713b4301a9355
TalionElessar2007 <cagnin.lima.2007@gmail.com>
2025-05-29 18:24:54 -0300

    Add vite-plugin-sitemap and configure sitemap generation

--- formula1/vite.config.js
+++ formula1/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
-// https://vite.dev/config/
+console.log('Vite config carregado! Verificando plugin sitemap...'); // ADICIONE ESTA LINHA
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://formula1-statistics.vercel.app/',
+      routes: [
+        '/',
+        '/pratices',
+        '/sprints',
+        '/drivers',
+        '/contact',
+        '/login',
+        '/register',
+        '/forgot',
+      ],
+    }),
+  ],
+});
