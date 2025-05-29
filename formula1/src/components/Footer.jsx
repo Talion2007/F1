7a07ece2f095feccf249fd105ae081119cd44109
TalionElessar2007 <cagnin.lima.2007@gmail.com>
2025-05-28 22:55:00 -0300

    V7

--- formula1/src/components/Footer.jsx
+++ formula1/src/components/Footer.jsx
@@ -3,13 +3,13 @@ function Footer() {
     return (
         <>
             <footer>
-                <h3>Cagnin Software Development</h3>
+                <h4>Cagnin Software Development</h4>
                 <p>© 2025 Cagnin Software Development. All rights reserved.</p>
                 <p>Developed by Felipe Cagnin | Full-Stack Developer in progress</p>
                 <div className="socials">
                     <a href="mailto:radiance.knight.2007@gmail.com">Email</a> 
                     <a href="https://github.com/Talion2007" target="_blank">GitHub</a> 
-                    <a href="https://linkedin.com/in/yourusername" target="_blank">LinkedIn</a>
+                    <a href="https://www.linkedin.com/in/felipe-cagnin-94a367348/" target="_blank">LinkedIn</a>
                 </div>
             </footer>
         </>
