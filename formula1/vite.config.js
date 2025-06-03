f89744838ef108e412a6a702b19200b02b3d7c1e
TalionElessar2007 <cagnin.lima.2007@gmail.com>
2025-06-02 21:26:04 -0300

    Update build script to copy sitemap.xml and remove robots.txt; modify sitemap structure

--- formula1/vite.config.js
+++ formula1/vite.config.js
@@ -1,30 +1,28 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import sitemap from 'vite-plugin-sitemap'; // Importe o plugin
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
   plugins: [
     react(),
     sitemap({
-      hostname: 'https://formula1-statistics.vercel.app/', // O domínio principal do seu site
+      hostname: 'https://formula1-statistics.vercel.app',
       routes: [
-        '/', // Sua página inicial
+        '/',
         '/pratices',
         '/sprints',
         '/drivers',
-        '/qualifying', // Adicionado!
+        '/qualifying',
         '/contact',
         '/login',
         '/register',
         '/forgot',
       ],
-      outDir: './dist', // Garante que o sitemap seja salvo na pasta 'dist'
+      outDir: './dist', // Pasta de saída padrão do build
     }),
   ],
-  // Adicione esta seção build para garantir a pasta de saída padrão
   build: {
     outDir: 'dist',
-    emptyOutDir: true // Limpa a pasta dist antes de cada build
-  }
+    emptyOutDir: true,
+  },
 });
